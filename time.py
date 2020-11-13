da93366d0b911ba3a1441a235402b201888d341b
Miguel Reyes <mreyes@life360.com>
2020-11-13 14:40:19 -0800

    Time playing

--- time.py
+++ time.py
@@ -7,3 +7,7 @@ print(now)
 sleep(0.2)
 later = time.time()
 print(later - now)
+
+print(time.strftime("%a, %d %b %Y %H:%M:%S UTC", time.gmtime()))
+print(time.strftime("%a, %d %b %Y %H:%M:%S local time"))
+print(int(time.strftime("%z"))/100) # UTC hours diff
